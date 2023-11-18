5040b9a4f843a5f192eda24dd3fe8f5e0e3065ab
ARTOMIA <nicaandrei2017@gmail.com>
2023-11-19 01:01:07 +0200

    5.3Version push

--- Source/UGJ_Aquinas_Protocol/UGJ_Aquinas_Protocol.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UGJ_Aquinas_Protocol.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UGJ_Aquinas_Protocol, "UGJ_Aquinas_Protocol" );
